800c9d0fb70e4caccdc26fa1b54839101a1fa35a
MaraSterian <mara.sterian93@gmail.com>
2026-06-30 17:17:50 +0300

    added lab directories, the .tex and .pdf files for the lab, an example of code

---
+++ index.md
@@ -0,0 +1,13 @@
+# Materiale Laborator
+
+- [Lab 1 - Linia de comandă și execuția](lab1/)
+- [Lab 2 - Funcții de sistem](lab2/)
+- [Lab 3 - Implementare funcții sistem](lab3/)
+- [Lab 4 - Procese (PDF)](lab4/suport-lab/lab4-PROCESE.pdf) | [materiale](lab4/materiale/)
+- [Lab 5 - Comunicare inter-proces - Semnale](lab5/)
+- [Lab 6 - Comunicare inter-proces - Memoria Partajată](lab6/)
+- [Lab 7 - PComunicare inter-proces - Pipes](lab7/)
+- [Lab 8 - Comunicare inter-proces - Sockets](lab8/)
+- [Lab 9 - Fire de execuție](lab9/)
+- [Lab 10 - Sincronizare: mutex, semafoare](lab10/)
+- [Lab 11 - Sincronizare: variabile condiționale, rwlocks](lab11/)
---
+++ lab4/materiale/fork_simplu.c
@@ -0,0 +1,34 @@
+#include <stdio.h> // standard input - output
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+void fork_simplu() {
+
+    printf("Inainte de fork - un singur proces cu PID: %d\n", getpid());
+
+    pid_t aux_pid = fork(); 
+    
+    /*
+     * de aici incolo ambele procese executa codul simultan
+     * procesele se comporta ca doua entitati diferite
+     */
+
+    if(aux_pid < 0) {
+        perror("fork"); // error pentru ca fork a esuat
+        //return 1;
+    }
+    else if (aux_pid == 0) {
+        // executat doar de copil
+        printf("PROCES COPIL cu PID: %d\n", getpid());
+        printf("Parintele meu are PID: %d\n", getppid());
+    }
+    else {
+        printf("PROCES PARINTE cu PID: %d\n", getpid());
+        printf("Copilul meu are PID: %d\n", aux_pid);
+    }
+
+}
